e0eb5ae2361d2c60a39be88e1a8803c8a60a3852
Ankit Singh Tanwar <ankitsingh957173@gmail.com>
2024-03-25 16:38:21 +0530

    complete signUp functionality setting error and loading

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,24 +1,83 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 export default function SignUp() {
-  return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>
-        Sign Up
-      </h1>
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    //isme puri url se bhi kr skte the lekin ab half url vite.config.js me daal diye as a proxy
 
-      <form className='flex flex-col gap-4' >
-        <input type="text" placeholder='username' id='username' className='bg-slate-100 p-3 rounded-lg' />
-        <input type="email" placeholder='Email' id='email' className='bg-slate-100 p-3 rounded-lg' />
-        <input type="password" placeholder='Password' id='password' className='bg-slate-100 p-3 rounded-lg' />
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setLoading(false);
+      //jaisa ki hum fetch use kr rhe h to hum setError ko ese set krenge
+      if (data.success == false) {
+        setError(true);
+        return;
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+    //reset the form data to empty on submit
+  };
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign Up"}
+        </button>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
         {/* link may be without { } also */}
-        <Link to={'/sign-in'}> <span className='text-blue-500'>Sign In</span></Link>
+        <Link to={"/sign-in"}>
+          {" "}
+          <span className="text-blue-500">Sign In</span>
+        </Link>
       </div>
+      <p className="text-red-700 mt-3">{error && "Something went wrong"}</p>
     </div>
-  )
+  );
 }
